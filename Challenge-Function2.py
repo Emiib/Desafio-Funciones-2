5d979dd42a40261ebb77e3d6c8236c1855ec3012
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2022-07-26 15:21:35 -0300

    otra solucion ejercicio 6

--- Challenge-Function2.py
+++ Challenge-Function2.py
@@ -87,3 +87,25 @@ def separar(lista):
 pares, impares = separar(lista)
 print(pares)
 print(impares)
+
+# Otra solucion EJERCICIO 6
+""" lista = [6,5,2,1,7]
+
+def separar(lista):
+    pares = []
+    impares = []
+
+    for i in lista:
+        if i%2 == 0:
+            pares.append(i)
+        else:
+            impares.append(i)
+
+    pares.sort()
+    impares.sort()
+
+    return pares, impares
+
+pares, impares = separar(lista)
+print(f"Los numeros PARES SON = {pares}")
+print(f"Los numeros IMPARES SON = {impares}") """
