399d54f63e068be9bdc1cfd5d9a68e23bb3c9728
Emiliano <emii88b@gmail.com>
2022-07-27 21:00:58 -0300

    git commit

---
+++ Ejercicio sin utilizar RECURSION.py
@@ -0,0 +1,8 @@
+# Ejercicio sin utilizar RECURSION
+
+# Ejercicio 4
+# Crear una función que reciba una lista que puede contener listas o números como elementos
+# La función debe devolver la suma de todos los números de todas las sublistas
+# Ej. [1,2,3,4] --> 10
+# Ej. [ [2,4] , [1], [4,2,1] ] --> 14
+# Ej. [ 2, [3,4], 5, [-3, [6 , [ 4,5 ] ] ] ] --> 26
